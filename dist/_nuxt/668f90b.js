40e2b736a68081311dfd977c1d35d3d5262b2bc4
Arthittapoom <118807101+Arthittapoom@users.noreply.github.com>
2025-03-05 00:59:39 +0700

    up

---
+++ dist/_nuxt/668f90b.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[18],{458:function(n,t,e){"use strict";e.r(t);var l={layout:"menu"},c=e(52),component=Object(c.a)(l,(function(){this._self._c;return this._m(0)}),[function(){var n=this._self._c;return n("div",[n("h1",[this._v("จัดการคาร์บอน")])])}],!1,null,null,null);t.default=component.exports}}]);
